f754d73672634367f8f13f4987799a9acb4e2575
guido_union6 <guido_union6@hotmail.com>
2024-07-18 19:43:02 -0300

    First Commit

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
+
+
+export const metadata: Metadata = {
+    title: "Coba App",
+    description: "The Coba group page"
+}
+
+const HomeLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+    return (
+        <main className="relative">
+            <NavBar />
+             <div className="flex">
+                <section className="flex min-h-screen flex-1 flex-col ">
+                    <div className="w-full">
+                        {children}
+                    </div>
+                </section>
+             </div>
+            <Footer />
+        </main>
+    );
+}
+
+export default HomeLayout
---
+++ app/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className="bg-transparent relative">
+      <div className="max-w-7xl min-h-[100vh] mx-auto flex justify-center items-center bh">
+        <div className='w-full'>
+          <video src="../assets/Video.mp4" width="1280" height="720" autoPlay controls>
+         </video>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
