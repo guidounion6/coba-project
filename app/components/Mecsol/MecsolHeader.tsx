81f51bf8c6c0c808b6f7ad9037c27e57105f963b
GContar <guidocontartese90@gmail.com>
2024-10-29 10:30:34 -0300

    Cambios

--- app/components/Mecsol/MecsolHeader.tsx
+++ app/components/Mecsol/MecsolHeader.tsx
@@ -19,8 +19,7 @@ const MecsolHeader = () => (
         </nav>
         <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#1556ac] text-[#f8fafb] text-sm font-bold leading-normal tracking-[0.015em]">
           <span className="truncate">Contactanos</span>
-        </button>
-        
+        </button> 
       </div>
     </header>
   );
